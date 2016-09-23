4696866afab0dd2ede31f658f75035613c066fa5
hexy <hexy@cdfortis.com>
2016-09-23 10:28:34 +0800

    add pod files

---
+++ Example/Pods/Target Support Files/XYShareSdk/XYShareSdk-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_XYShareSdk : NSObject
+@end
+@implementation PodsDummy_XYShareSdk
+@end
---
+++ Example/Pods/Target Support Files/XYShareSdk/XYShareSdk-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double XYShareSdkVersionNumber;
+FOUNDATION_EXPORT const unsigned char XYShareSdkVersionString[];
+
